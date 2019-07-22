f220acca233cd17e5c805509790490547854c929
nuso <nelohjb@hotmail.com>
2019-07-22 16:14:23 +0800

    init & custom inspector

---
+++ Assets/Editor/CubeEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Cube))]
+public class CubeEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("COLORIT")) {
+            ((Cube)target).RandomColor();
+        }
+        if (GUILayout.Button("Reset")) {
+            ((Cube)target).ResetColor();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public void RandomColor() {
+        Renderer renderer = GetComponent<MeshRenderer>();
+        if (renderer != null) {
+            renderer.material.color = Random.ColorHSV();
+        }
+    }
+
+    public void ResetColor() {
+        Renderer renderer = GetComponent<MeshRenderer>();
+        if (renderer != null) {
+            renderer.material.color = Color.white;
+        }
+    }
+}
