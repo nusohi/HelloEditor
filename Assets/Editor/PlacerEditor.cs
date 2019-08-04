48da0f8bb1071438bd4c7519e0b7494412471a5c
nuso <nelohjb@hotmail.com>
2019-08-04 13:59:39 +0800

    计算曲线平均分割点，PathPlacer

---
+++ Assets/Editor/PlacerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathPlacer))]
+public class PlacerEditor : Editor
+{
+    private PathPlacer placer;
+
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Placer")) {
+            placer.Placer();
+        }
+    }
+
+    private void OnEnable() {
+        placer = (PathPlacer)target;
+    }
+}
---
+++ Assets/Scripts/PathPlacer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPlacer : MonoBehaviour
+{
+    private PathCreator creator;
+    private Transform spacingParent;
+    [Range(0,1)]
+    public float Spacing = 0.1f;
+    public float Resolution = 1f;
+    public float Scale = 0.5f;
+
+    void Awake()
+    {
+        creator = GetComponent<PathCreator>();
+        spacingParent = gameObject.transform.Find("Spacings");
+        Placer();
+    }
+
+    public void Placer() {
+        DestroyPlacer();
+
+        Vector2[] points = creator.path.CalEvenSpacedPoints(Spacing, Resolution);
+        foreach(Vector2 point in points) {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            go.transform.parent = spacingParent;
+            go.transform.position = point;
+            go.transform.localScale = Vector3.one * Spacing * Scale;
+        }
+    }
+
+    public void DestroyPlacer() {
+        for (int i = 0; i < spacingParent.childCount; i++) {
+            Destroy(spacingParent.GetChild(i).gameObject);
+        }
+    }
+}
